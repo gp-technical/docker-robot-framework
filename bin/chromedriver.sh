63f0a67034002df8e3f8a76097ed85743ffbe963
Marcelo Marra <marcelo@devsquad.email>
2019-05-30 07:54:28 -0300

    Adding extra parameters to chrome and chromium

--- bin/chromedriver.sh
+++ bin/chromedriver.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-/usr/bin/chromedriver --verbose --log-path=/var/log/chromedriver --no-sandbox $@
+/usr/bin/chromedriver --verbose --log-path=/var/log/chromedriver --disable-gpu --disable-impl-side-painting --disable-gpu-sandbox --disable-accelerated-2d-canvas --disable-accelerated-jpeg-decoding --no-sandbox --test-type=ui $@
 
--- bin/chromium-browser.sh
+++ bin/chromium-browser.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-/usr/lib64/chromium-browser/chromium-browser-original --disable-gpu --no-sandbox $@
+/usr/lib64/chromium-browser/chromium-browser-original  --verbose --log-path=/var/log/chromiumdriver --disable-gpu --disable-impl-side-painting --disable-gpu-sandbox --disable-accelerated-2d-canvas --disable-accelerated-jpeg-decoding --no-sandbox --test-type=ui $@
 
